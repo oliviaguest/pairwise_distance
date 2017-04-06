8fa89909f13b06a85d029a24099465e4f55587f2
Olivia Guest <guest.olivia@gmail.com>
2017-04-06 19:21:21 +0100

    adding comments and considering next thing: weights

--- pairwise_distance.py
+++ pairwise_distance.py
@@ -1,45 +1,49 @@
 from __future__ import division
 
-import multiprocessing
 import sklearn.datasets
-import multiprocessing
 import scipy.spatial.distance
 
 import numpy as np
+import multiprocessing as mp
 
 from time import time
 from itertools import combinations
 
-
-def do_job(job_id, data_slice, q):
+def do_job(data_slice, q):
     partial_sum = 0
-    for item in data_slice:
-        partial_sum += np.sum(np.sqrt(np.sum((item[0] - item[1])**2, axis=1)))
+    for points in data_slice:
+        # Each data_slice has a tuple of two points of which we need to find the
+        # Euclidean distance between:
+        partial_sum += np.sum(np.sqrt(np.sum((points[0] - points[1])**2, axis=1)))
     q.put(partial_sum)
 
-
-def dispatch_jobs(data, job_number):
-    total = len(data)
-    slices = np.array_split(data, job_number)
+def dispatch_jobs(X, job_number):
+    N = X.shape[0]
+    # Get the pairs to calculate the distances without needing the whole of X:
+    pairs = [(X[i:], X[:N - i]) for i in xrange(1, N)]
+    # Create slices of the pairs to send to each worker:
+    slices = np.array_split(pairs, job_number)
     jobs = []
     queues = []
     for i, s in enumerate(slices):
-        queues.append(multiprocessing.Queue())
-
-        j = multiprocessing.Process(target=do_job, args=(i, s, queues[i]))
-        jobs.append(j)
+        # We need queues to get the data back from each worker function:
+        queues.append(mp.Queue())
+        # Create a job and append it to the list of jobs:
+        jobs.append(mp.Process(
+            target=do_job, args=(s, queues[i])))
+        # print i, s
+    # Start all the jobs:
     for j in jobs:
         j.start()
+    # Calculate the sum:
     queue_sum = 0
     for q in queues:
         queue_sum += q.get()
-    print queue_sum
     return queue_sum
 
-
 if __name__ == "__main__":
-    ####### Generate some data ###############################################
-    N = 100
+    # Generate some data:
+    N = 5
     centers = [[0, 0], [1, 0], [0.5, np.sqrt(0.75)]]
     cluster_std = [0.3, 0.3, 0.3]
     n_clusters = len(centers)
@@ -52,17 +56,15 @@ if __name__ == "__main__":
     extra, labels_true = sklearn.datasets.make_blobs(n_samples=int(0.25 * N),
                                                      centers=centers, cluster_std=cluster_std)
     X = np.concatenate((data, extra), axis=0)
-    N = X.shape[0]
-    # Get the pairs to calculate the distances ###############################
-    pairs = [(X[i:], X[:N - i]) for i in xrange(1, N)]
     # Do the thing ###########################################################
-    # I have 32 cores, this gives good performance.
-    my_sum = dispatch_jobs(pairs, multiprocessing.cpu_count() - 4)
-    # Comment this out if you use a high N as it will eat RAM! ###############
+    # I have 32 cores, this gives good performance:
+    my_sum = dispatch_jobs(X, mp.cpu_count() - 4)
+    # Comment this out if you use a high N as it will eat RAM!
+    # SERIOUSLY, be careful.
     Y = scipy.spatial.distance.pdist(X, 'euclidean')
-    print np.sum(Y)
+    print np.sum(Y), my_sum
     assert np.round(np.sum(Y)) == np.round(
-        my_sum)  # there is minor rounding error
+        my_sum) # There is minor rounding error after 8 decimal places.
 
     # Backstory of ideas: ####################################################
     ##########################################################################
