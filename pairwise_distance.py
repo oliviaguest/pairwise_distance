85e5d429d483985beda2b40658391ef8cf9278d5
Grant R. Vousden-Dishington <GrantRVD@gmail.com>
2017-04-09 19:58:16 -0700

    (Minor) optimizations and code beautification.

--- pairwise_distance.py
+++ pairwise_distance.py
@@ -2,12 +2,11 @@ from __future__ import division
 from time import time
 from math import factorial
 from itertools import combinations
+import multiprocessing as mp
 
 import sklearn.datasets
 import scipy.spatial.distance
-
 import numpy as np
-import multiprocessing as mp
 
 ##############################
 # Code: Olivia Guest         #
@@ -15,17 +14,13 @@ import multiprocessing as mp
 ##############################
 
 
-def do_job(data_slice, job_index, queue):
-    # print job_index, data_slice.shape
-    partial_sum = 0
-    for i, points in enumerate(data_slice):
-        # Each data_slice has tuples consisting of two points that we need to
-        # find the Euclidean distance between and their weight:
-        # points[2] are the weights for the pair points[0] and points[1]
-        partial_sum += np.sum(points[2] *
-                              np.sqrt(np.sum((points[0] - points[1])**2,
-                                      axis=1)))
-    queue.put(partial_sum)
+def do_job(data_slice, queue):
+    # Each data_slice has tuples consisting of two points that we need to
+    # find the Euclidean distance between and their weight:
+    # points[2] are the weights for the pair points[0] and points[1]
+    slice_sum = np.sum(np.dot(weights, np.sqrt(np.sum((X - Y)**2, axis=1)))
+                       for X, Y, weights in data_slice)
+    queue.put(slice_sum)
 
 # If you want to memory profile this funtion to see it is roughly constant,
 # feel free to comment in the decorator and run with memory_profiler (install
@@ -34,7 +29,7 @@ def do_job(data_slice, job_index, queue):
 # @profile
 
 
-def mean_pairwise_distance(X, weights=None, n_jobs=None):
+def mean_pairwise_distance(X, weights=None, n_jobs=None, axis=0):
     """Function that returns the sum and mean of the pairwise distances of an 2D
     array X.
 
@@ -43,34 +38,32 @@ def mean_pairwise_distance(X, weights=None, n_jobs=None):
 
     Optional arguments:
     weights -- 1D array of counts or weights per point in X (default: 1s).
-    n_jobs  -- Numper of cores to use for calulation (default: all).
+    n_jobs  -- Number of cores to use for calulation (default: all).
+    axis    -- The axis of X corresponding to data elements (default: 0).
     """
-    N = X.shape[0]
+    N = X.shape[axis]
     if weights is None:
         weights = np.ones((N,))
     if n_jobs is None:
         n_jobs = mp.cpu_count()
+
     # Get the pairs and their weights to calculate the distances without
     # needing the whole of X:
     pairs = [(X[i:], X[:N - i], weights[i:] * weights[:N - i])
              for i in xrange(1, N)]
-    # Create slices of the pairs to send to each worker:
+    # Create approximately equal splits of the pairs for each cpu:
     pairs_slices = np.array_split(pairs, n_jobs)
-    jobs = []
-    queues = []
-    for i, pairs_slice in enumerate(pairs_slices):
-        # We need queues to get the data back from each worker function:
-        queues.append(mp.Queue())
-        # Create a job and append it to the list of jobs:
-        jobs.append(mp.Process(
-            target=do_job, args=(pairs_slice, i, queues[i])))
-    # Start all the jobs:
+
+    # Make queues to collect results and processes to do the calculations:
+    queues = [mp.Queue() for i in xrange(n_jobs)]
+    jobs = [mp.Process(target=do_job, args=(pairs_slice, queue))
+            for queue, pairs_slice in zip(queues, pairs_slices)]
+
+    # Start all the jobs, aggregate the sum when they finish:
     for j in jobs:
         j.start()
-    # Calculate the sum:
-    queue_sum = 0
-    for q in queues:
-        queue_sum += q.get()
+    queue_sum = sum(q.get() for q in queues)
+
     # Compute the number of combinations, add to the number of unique pairs
     # and use that as the denominator to calculate the mean pairwise distance:
     mean = queue_sum / (((N - 1)**2 + (N + 1)) / 2 + N)
@@ -81,21 +74,28 @@ def mean_pairwise_distance(X, weights=None, n_jobs=None):
 if __name__ == "__main__":
     # Generate some data:
     N = 1000
-    centers = [[0, 0], [1, 0], [0.5, np.sqrt(0.75)]]
-    cluster_std = [0.3, 0.3, 0.3]
+    np.random.seed(10)
+    n_jobs = mp.cpu_count()
+    n_samples = N * 3 // 4  # same as floor(3/4 * N)
+
+    # Blob set 1
+    centers = [[0., 0.],
+               [1., 0.],
+               [0.5, np.sqrt(0.75)]]
     n_clusters = len(centers)
-    n_samples = int(0.75 * N)
+    cluster_std = [0.3] * n_clusters
     data, labels_true = sklearn.datasets.make_blobs(n_samples=n_samples,
                                                     centers=centers,
                                                     cluster_std=cluster_std)
+
+    # Blob set 2
     centers = [[0.5, np.sqrt(0.75)]]
-    cluster_std = [0.3]
     n_clusters = len(centers)
-    extra, labels_true = sklearn.datasets.make_blobs(n_samples=int(0.25 * N),
+    cluster_std = [0.3] * n_clusters
+    extra, labels_true = sklearn.datasets.make_blobs(n_samples=N - n_samples,
                                                      centers=centers,
                                                      cluster_std=cluster_std)
     X = np.concatenate((data, extra), axis=0)
-    N = X.shape[0]
 
     # Pick some random floats for the counts/weights:
     counts = np.random.random_sample((N,)) * 10
@@ -103,24 +103,24 @@ if __name__ == "__main__":
     # Parallel:
     # Parallelised code partially based on:
     # https://gist.github.com/baojie/6047780
-    t = time()
+    t_start = time()
     parallel_sum, parallel_mean = mean_pairwise_distance(X,
                                                          weights=counts,
-                                                         n_jobs=mp.cpu_count()
-                                                         )
-    print 'parallel:\t{} s'.format(time() - t)
+                                                         n_jobs=n_jobs)
+    t_end = time()
+    print 'parallel:\t{} s'.format(t_end - t_start)
     ##########################################################################
 
     ##########################################################################
     # Serial:
     # Comment this out if you use a high N as it will eat RAM!
-    t = time()
-    Y = scipy.spatial.distance.pdist(X, 'euclidean')
-    weights = [counts[i] * counts[j]
-               for i in xrange(N - 1) for j in xrange(i + 1, N)]
-    serial_sum = np.sum(weights * Y)
+    t_start = time()
+    weights = np.array([counts[i] * counts[j]
+                       for i in xrange(N - 1) for j in xrange(i + 1, N)])
+    serial_sum = np.dot(weights, scipy.spatial.distance.pdist(X, 'euclidean'))
     serial_mean = serial_sum / (((N - 1)**2 + (N + 1)) / 2 + N)
-    print 'serial:\t\t{} s'.format(time() - t)
+    t_end = time()
+    print 'serial:\t\t{} s'.format(t_end - t_start)
     ##########################################################################
 
     # There is minor rounding error, but check for equality:
