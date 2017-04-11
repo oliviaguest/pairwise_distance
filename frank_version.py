25ca01903f1f6abc4da1e5fb6a315ecf2c6aa00a
Olivia Guest <guest.olivia@gmail.com>
2017-04-11 10:35:41 +0100

    tidy

--- frank_version.py
+++ frank_version.py
@@ -9,18 +9,18 @@ def ByClusterDistParallelHelper(dataTask):
 	dataSegment=dataTask[0]
 	helperDist=0
 	# print("dataTask",dataTask[1:4])
-	peopleInCluster=len(dataSegment)
+	pointsInCluster=len(dataSegment)
 	for j in xrange(dataTask[1],dataTask[2],dataTask[3]):
-		helperDist+=sum(np.sqrt(np.add.reduce((dataSegment.iloc[(j+1):peopleInCluster]-dataSegment.iloc[j])**2,1)))
+		helperDist+=sum(np.sqrt(np.add.reduce((dataSegment.iloc[(j+1):pointsInCluster]-dataSegment.iloc[j])**2,1)))
 	return helperDist
 
 #############################
 # This function should be ready for prime time use in project
 ### what is the structure of data??? data should be called with M
-def ByClusterDistParallel(data,step_size,label_name,numClusters,RealpeoplePerCluster):
+def ByClusterDistParallel(data,step_size,label_name,numClusters,RealpointsPerCluster):
     #some defintions
     pool = multiprocessing.Pool(None) #this defines the pool of processors/cores.
-    peoplePerCluster=np.zeros(numClusters,int) #number of people in each cluster
+    pointsPerCluster=np.zeros(numClusters,int) #number of points in each cluster
     totalDist=0
     totalDistPerCluster=np.zeros(numClusters,float) #sum of pairwise distances for each cluster
     ################ same in next line... numClusters
@@ -29,18 +29,18 @@ def ByClusterDistParallel(data,step_size,label_name,numClusters,RealpeoplePerClu
         temp = data.loc[data[label_name]==clus_index,'x':'y']
 
         temp=temp.iloc[range(0,len(temp),step_size)] #**** try shrinking the data matrix here by just discarding some data.
-        peoplePerCluster[i]=len(temp)
+        pointsPerCluster[i]=len(temp)
         #############    	create split of tasks for multiprocessing.
         tasks=[]
-        taskSize=peoplePerCluster[i]/pool._processes
+        taskSize=pointsPerCluster[i]/pool._processes
         for j in xrange(pool._processes):
             tasks.append([temp,j*taskSize,(j+1)*taskSize,1]) #step_size removed.
-        tasks[j][2]=peoplePerCluster[i]-1 	#this ensures the last task gets any extra points resulting from dividing by pool.processes
+        tasks[j][2]=pointsPerCluster[i]-1 	#this ensures the last task gets any extra points resulting from dividing by pool.processes
         ############
         results = []
         r = pool.map_async(ByClusterDistParallelHelper, tasks, callback=results.append) #splits up the tasks across processors.
         r.wait() # Wait on the results
         totalDist+=np.sum(results) #this will be removed.
         totalDistPerCluster[i]=np.sum(results)
-    avgDistPerCluster=(totalDistPerCluster)/(peoplePerCluster*(peoplePerCluster-1)/2.)  #step_size no longer in numerator
-    return avgDistPerCluster, peoplePerCluster
+    avgDistPerCluster=(totalDistPerCluster)/(pointsPerCluster*(pointsPerCluster-1)/2.)  #step_size no longer in numerator
+    return avgDistPerCluster, pointsPerCluster
