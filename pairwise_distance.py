d6c5742cddcb14897272025b3617dcd0a1eaa1ff
Grant R. Vousden-Dishington <GrantRVD@gmail.com>
2017-04-11 16:09:08 -0700

    Refactor code to remove unnecessary imports and for easy testing with pytest. All tests can now be run with python -m pytest -s ============================= test session starts ============================== platform linux2 -- Python 2.7.9, pytest-3.0.7, py-1.4.33, pluggy-0.4.0 rootdir: /home/grant/github/justforkicks/pairwise_distance, inifile: collected 1 items
    tests/test_pairwise_distance.py
      parallel:   0.219860 s
        serial:   0.185895 s
    sum = 10835487.3218
    mean = 21.6492820631
    .
    
    =========================== 1 passed in 0.71 seconds =========================== command. Usage notes added to README.

--- pairwise_distance.py
+++ pairwise_distance.py
@@ -1,11 +1,6 @@
 from __future__ import division
-from time import time
-from math import factorial
-from itertools import combinations
 import multiprocessing as mp
 
-import sklearn.datasets
-import scipy.spatial.distance
 import numpy as np
 
 ##############################
@@ -20,13 +15,6 @@ def batch_pdist(data_slice):
     # points[2] are the weights for the pair points[0] and points[1]
     return np.sum(np.dot(weights, np.sqrt(np.sum((X - Y)**2, axis=1)))
                   for X, Y, weights in data_slice)
-
-# If you want to memory profile this funtion to see it is roughly constant,
-# feel free to comment in the decorator and run with memory_profiler (install
-# it) as below:
-# python -m memory_profiler pairwise_distance.py
-# @profile
-
 
 def mean_pairwise_distance(X, weights=None, n_jobs=None, axis=0):
     """Function that returns the sum and mean of the pairwise distances of an 2D
@@ -66,61 +54,3 @@ def mean_pairwise_distance(X, weights=None, n_jobs=None, axis=0):
     # mean = queue_sum / (((N - 1)**2 + (N + 1)) / 2.0)
 
     return queue_sum, mean
-
-if __name__ == "__main__":
-    # Generate some data:
-    N = 1000
-    np.random.seed(10)
-    n_jobs = mp.cpu_count()
-    n_samples = N * 3 // 4  # same as floor(3/4 * N)
-
-    # Blob set 1
-    centers = [[0., 0.],
-               [1., 0.],
-               [0.5, np.sqrt(0.75)]]
-    n_clusters = len(centers)
-    cluster_std = [0.3] * n_clusters
-    data, labels_true = sklearn.datasets.make_blobs(n_samples=n_samples,
-                                                    centers=centers,
-                                                    cluster_std=cluster_std)
-
-    # Blob set 2
-    centers = [[0.5, np.sqrt(0.75)]]
-    n_clusters = len(centers)
-    cluster_std = [0.3] * n_clusters
-    extra, labels_true = sklearn.datasets.make_blobs(n_samples=N - n_samples,
-                                                     centers=centers,
-                                                     cluster_std=cluster_std)
-    X = np.concatenate((data, extra), axis=0)
-
-    # Pick some random floats for the counts/weights:
-    counts = np.random.random_sample((N,)) * 10
-    ##########################################################################
-    # Parallel:
-    # Parallelised code partially based on:
-    # https://gist.github.com/baojie/6047780
-    t_start = time()
-    parallel_sum, parallel_mean = mean_pairwise_distance(X,
-                                                         weights=counts,
-                                                         n_jobs=n_jobs)
-    t_end = time()
-    print 'parallel:\t{} s'.format(t_end - t_start)
-    ##########################################################################
-
-    ##########################################################################
-    # Serial:
-    # Comment this out if you use a high N as it will eat RAM!
-    t_start = time()
-    weights = np.array([counts[i] * counts[j]
-                       for i in xrange(N - 1) for j in xrange(i + 1, N)])
-    serial_sum = np.dot(weights, scipy.spatial.distance.pdist(X, 'euclidean'))
-    serial_mean = serial_sum / (((N - 1)**2 + (N + 1)) / 2 + N)
-    t_end = time()
-    print 'serial:\t\t{} s'.format(t_end - t_start)
-    ##########################################################################
-
-    # There is minor rounding error, but check for equality:
-    assert np.round(serial_sum) == np.round(parallel_sum)
-    assert np.round(serial_mean) == np.round(parallel_mean)
-    print 'sum = {}'.format(parallel_sum)
-    print 'mean = {}'.format(parallel_mean)
